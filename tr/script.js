c982878cf0cde1353684816346cfa3fc3f1600e1
ahmetmumcuoglu <128048095+ahmetmumcuoglu@users.noreply.github.com>
2026-01-07 16:19:39 +0300

    Update script.js

--- tr/script.js
+++ tr/script.js
@@ -10778,13 +10778,14 @@ function animateSlotScore(targetNumber, containerId) {
         // Küçük bir gecikmeyle (delay) haneleri döndür
         setTimeout(() => {
             const finalDigit = parseInt(digit);
-            // Son rakamı bulmak için: Her hane 60px boyunda. 
+            // Son rakamı bulmak için: Her hane 35px boyunda. 
             // 10 + finalDigit diyerek en az bir tur dönmesini garanti ediyoruz.
-            const moveY = (10 + finalDigit) * 45;
+            const moveY = (10 + finalDigit) * 35;
             column.style.transform = `translateY(-${moveY}px)`;
         }, index * 150); // Her hane 150ms arayla dönmeye başlar (Slottaki gibi)
     });
 }
+
 
 
 
